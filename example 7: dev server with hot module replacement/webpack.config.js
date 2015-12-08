2f726c5df707fc723c14fbb7ac20cf85e575a849
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-12-08 18:08:04 +0100

    tabs

--- example 7: dev server with hot module replacement/webpack.config.js
+++ example 7: dev server with hot module replacement/webpack.config.js
@@ -4,29 +4,29 @@ var webpack = require('webpack');
 var APP_PATH = path.join(__dirname, "/src");
 
 module.exports = {
-    context: APP_PATH,
-    entry: "./",
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "[name].js",
-        publicPath: 'http://localhost:3000/dist'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loaders: ["style", "css"],
-                include: APP_PATH
-            }
-        ]
-    },
-    devServer: {
-        port: 3000,
-        stats: { colors: true },
-        inline: true,
-        publicPath: "/dist/"
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+  context: APP_PATH,
+  entry: "./",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: 'http://localhost:3000/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ["style", "css"],
+        include: APP_PATH
+      }
     ]
+  },
+    devServer: {
+    port: 3000,
+    stats: { colors: true },
+    inline: true,
+    publicPath: "/dist/"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
